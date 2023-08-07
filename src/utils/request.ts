ff8deda6e8e3876fc13171e0a1a5ff2e94b6b29d
lwnmengjing <lwnmengjing@qq.com>
2023-08-08 00:40:49 +0800

    :tada: init project

---
+++ src/utils/request.ts
@@ -0,0 +1,88 @@
+import axios from 'axios'
+import { Message, Modal } from '@arco-design/web-react';
+
+// create an axios instance
+const api = axios.create({
+  baseURL: typeof import.meta.env.REACT_APP_API_URL === "string" ? import.meta.env.REACT_APP_API_URL : '',
+  // baseURL: '',
+  // withCredentials: true, // send cookies when cross-domain requests
+  // baseURL: '',
+  timeout: 50000 // request timeout
+})
+
+// request interceptor
+api.interceptors.request.use(
+  config => {
+    // do something before request is sent
+    //todo set access token
+    // if (store.getters.token) {
+    //   config.headers['Authorization'] = 'Bearer ' + getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+    // }
+    console.log('url:' + config.url);
+    return config
+  },
+  error => {
+    // do something with request error
+    console.log(error) // for debug
+    return Promise.reject(error)
+  }
+)
+
+// response interceptor
+api.interceptors.response.use(
+  /**
+   * If you want to get http information such as headers or status
+   * Please return  response => response
+  */
+
+  /**
+   * Determine the request status by custom code
+   * Here is just an example
+   * You can also judge the status by HTTP Status Code
+   */
+  response => {
+    console.log('response:' + JSON.stringify(response))
+    const res = response.data
+
+    // if the custom code is not 20000, it is judged as an error.
+    if (res.code > 399) {
+      // console.log('response111111:' + JSON.stringify(response))
+    //   Message.error(res.detail || 'Error');
+        switch (res.code) {
+        case 401:
+          // todo re-login
+					Modal.confirm({
+						title: 'Re-Login',
+						content: 'You have been logged out, you can cancel to stay on this page, or log in again',
+						okButtonProps: {
+							status: 'danger',
+						},
+						onOk: () => {
+							//todo clean accessToken
+              console.log('clean accessToken')
+						},
+					});
+          break;
+        case 422:
+          Message.error(res.detail ? '输入信息有误:' + res.detail : '输入信息有误');
+          break;
+        default:
+          Message.error(res.detail || 'Error');
+          break;
+        }
+      return Promise.reject(new Error(res.detail || 'Error'))
+    } else {
+      return response
+    }
+  },
+  error => {
+    // console.log('err' + JSON.stringify(error)) // for debug
+    const response = error.response
+    const { data } = response
+    //todo
+		Message.error(data.msg || 'Error');
+    return Promise.reject(error)
+  }
+)
+
+export default api
